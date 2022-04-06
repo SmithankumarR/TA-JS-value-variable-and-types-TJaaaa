a3afdec03546dcec04198556eff8763ccf5c9a26
smithan kumar r <smithankumar404@gmail.com>
2022-04-06 13:57:37 +0530

    for loop & while loop

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,11 +3,30 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
 
 // Using `console.log` log all the evan values from 1 to 10.
-
+// for(let i=0; i<=10; i = i+2){
+//     console.log(i);
+// }
 // Using `console.log` log all the od values from 1 to 10.
+// for(let i=1; i<10; i= i+2){
+// console.log(i);
+// }
 
 // Calculate the sum of all numbers from 1 to 10.
+let sum = 0;
+for(let i=0; i<10; i++){
+    sum= sum + i;
+    console.log(sum);
+}
+
 
 // Log all the values from 1 to 10 using while loop
+// let v=0;
+// while(v<= 10){
+// console.log(v);
+// v=v+1;
+// }
